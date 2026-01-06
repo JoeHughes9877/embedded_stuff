bf81900781b05c316b469611fe682276e858221d
JoeHughes9877 <joeydarlo1000@gmail.com>
2026-01-06 13:39:44 +0000

    better traffic blinky boop

--- blinky/main.c
+++ blinky/main.c
@@ -6,30 +6,22 @@
 
 int main(void) {
 
+  const uint8_t leds[] = { PB4, PB3, PB2 };
+
+  uint8_t amountOfLeds = sizeof(leds) / sizeof(leds[0]);
+
   DDRB |= (1 << PB4); //red light
   DDRB |= (1 << PB3); //yellow light
   DDRB |= (1 << PB2); //green light
 
   while (1) {
-    PORTB |= (1 << PB4);
-    _delay_ms(BLINK_MS);
+    for (uint8_t i = 0; i < amountOfLeds; i++) {
+      PORTB |= (1 << leds[i]);
+      _delay_ms(BLINK_MS);
 
-    PORTB &= ~(1 << PB4);
-    _delay_ms(BLINK_MS);
-
-
-    PORTB |= (1 << PB3);
-    _delay_ms(BLINK_MS);
-
-    PORTB &= ~(1 << PB3);
-    _delay_ms(BLINK_MS);
-
-
-    PORTB |= (1 << PB2);
-    _delay_ms(BLINK_MS);
-
-    PORTB &= ~(1 << PB2);
-    _delay_ms(BLINK_MS);
+      PORTB &= ~(1 << leds[i]);
+      _delay_ms(BLINK_MS);
+    }
   }
 
   return 0;
