3995e30f8c07cb84f10f02191b0e3c66fcb8c490
JoeHughes9877 <joeydarlo1000@gmail.com>
2026-01-06 00:58:23 +0000

    Traffic blinky boop

--- blinky/main.c
+++ blinky/main.c
@@ -2,17 +2,33 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#define BLINK_MS 150
+#define BLINK_MS 300
 
 int main(void) {
 
-  DDRB |= (1 << PB4);
+  DDRB |= (1 << PB4); //red light
+  DDRB |= (1 << PB3); //yellow light
+  DDRB |= (1 << PB2); //green light
 
   while (1) {
     PORTB |= (1 << PB4);
     _delay_ms(BLINK_MS);
 
     PORTB &= ~(1 << PB4);
+    _delay_ms(BLINK_MS);
+
+
+    PORTB |= (1 << PB3);
+    _delay_ms(BLINK_MS);
+
+    PORTB &= ~(1 << PB3);
+    _delay_ms(BLINK_MS);
+
+
+    PORTB |= (1 << PB2);
+    _delay_ms(BLINK_MS);
+
+    PORTB &= ~(1 << PB2);
     _delay_ms(BLINK_MS);
   }
 
