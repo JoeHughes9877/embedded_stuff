15097100ddac11eb2d98ceea41a52e02c8a4dec6
JoeHughes9877 <joeydarlo1000@gmail.com>
2026-01-14 21:17:50 +0000

    ooopiimmiissaattiioonnss

--- blinky/main.c
+++ blinky/main.c
@@ -4,25 +4,42 @@
 
 #define BLINK_MS 300
 
+#define OK 0
+#define ERR 1
+
+short init_GPIO(volatile uint8_t* ddr, uint8_t pin);
+
 int main(void) {
 
-  const uint8_t leds[] = { PB4, PB3, PB2 };
+  const uint8_t leds[] = { PD7, PB3, PB2 };
+  volatile uint8_t* ports[] = {&PORTD, &PORTB, &PORTB};
+
 
   uint8_t amountOfLeds = sizeof(leds) / sizeof(leds[0]);
 
-  DDRB |= (1 << PB4); //red light
-  DDRB |= (1 << PB3); //yellow light
-  DDRB |= (1 << PB2); //green light
+  init_GPIO(&DDRD, PD7);
+  init_GPIO(&DDRB, PB3);
+  init_GPIO(&DDRB, PB2);
 
   while (1) {
     for (uint8_t i = 0; i < amountOfLeds; i++) {
-      PORTB |= (1 << leds[i]);
+      *ports[i] |= (1 << leds[i]);
       _delay_ms(BLINK_MS);
 
-      PORTB &= ~(1 << leds[i]);
+      *ports[i] &= ~(1 << leds[i]);
       _delay_ms(BLINK_MS);
     }
   }
 
   return 0;
+}
+
+short init_GPIO(volatile uint8_t* ddr, uint8_t pin) {
+  //*ddr allows for both B and D pins to be processed via pointer
+  *ddr |= (1 << pin);
+  if ((*ddr & (1 << pin)) != 0) {
+    return OK;
+  }else {
+    return ERR;
+  }
 }
