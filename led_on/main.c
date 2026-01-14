a8d04b1fd13ccb3ce3dac12d9be517e85cff2ff9
JoeHughes9877 <joeydarlo1000@gmail.com>
2026-01-14 21:56:09 +0000

    implicit

--- led_on/main.c
+++ led_on/main.c
@@ -1,10 +1,18 @@
 #include <avr/io.h>
 #include <stdbool.h>
 
+#define OK 0
+#define ERR 1
+
+short init_GPIO(volatile uint8_t* ddr, uint8_t pin);
+
 int main(void) {
     DDRB |= (1 << PB4); //red light
-    DDRB &= ~(1 << PB2); //button
     PORTB |= (1 << PB2); 
+
+    init_GPIO(&DDRB, PB4);
+    init_GPIO(&DDRB, PB2);
+    DDRB &= ~(1 << PB2); //button
 
     bool previousState = false;
 
@@ -18,4 +26,14 @@ int main(void) {
     previousState = currentState;
   }
   return 0;
+}
+
+short init_GPIO(volatile uint8_t* ddr, uint8_t pin) {
+  //*ddr allows for both B and D pins to be processed via pointer
+  *ddr |= (1 << pin);
+  if ((*ddr & (1 << pin)) != 0) {
+    return OK;
+  }else {
+    return ERR;
+  }
 }
